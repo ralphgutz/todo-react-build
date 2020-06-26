bffa1c89f644a16146ebfbd112d56def9112f8fc
Raphael Gutierrez <urd1_17@yahoo.com>
2020-06-26 23:12:35 +0800

    first commit

---
+++ precache-manifest.a6b49dd846a0390fd11ed3b82870cdf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f19718e15ff39ffab9b42652a74e808",
+    "url": "/todo-react-build/index.html"
+  },
+  {
+    "revision": "fe8cbf4cdb77a7a1ba9c",
+    "url": "/todo-react-build/static/css/main.2f4f5518.chunk.css"
+  },
+  {
+    "revision": "2b325f4ac475d4ddca01",
+    "url": "/todo-react-build/static/js/2.1c06bc53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react-build/static/js/2.1c06bc53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe8cbf4cdb77a7a1ba9c",
+    "url": "/todo-react-build/static/js/main.26d14e09.chunk.js"
+  },
+  {
+    "revision": "ae16dade2de9c812d70a",
+    "url": "/todo-react-build/static/js/runtime-main.cb00bc73.js"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/todo-react-build/static/media/Poppins-Regular.41e8dead.ttf"
+  }
+]);
